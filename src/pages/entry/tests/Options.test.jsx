07ba77e67db542675622c6292c5964364fd787d7
M Natoli <natoli.missy@gmail.com>
2022-03-25 12:44:04 -0500

    topping tests and code

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -6,9 +6,21 @@ test("displays image for each scoop from the server", async () => {
 
     // find images
     const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
-    expect(scoopImages).toHaveLength(2); // because we have 2 in our handlers array 
+    expect(scoopImages).toHaveLength(2); // because we have 2 in our handlers array  
 
     // confirm alt text of images
     const altText = scoopImages.map((element) => element.alt);
     expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test('display image for each topping from the server', async () => {
+    render (<Options optionType="toppings" />);
+
+    //find images
+    const toppingImages = await screen.findAllByRole("img", { name: /topping$/i});
+    expect(toppingImages).toHaveLength(3);
+
+    // confirm alt text of images
+    const imageTitles = toppingImages.map((img) => img.alt);
+    expect(imageTitles).toEqual(["Cherries topping", "M&Ms topping", "Hot fudge topping"]);
+})
