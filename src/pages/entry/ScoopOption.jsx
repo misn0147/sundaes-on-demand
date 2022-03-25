50de95b17afc046c2411168a544cd483728f97ad
M Natoli <natoli.missy@gmail.com>
2022-03-25 12:30:47 -0500

    debugged image issue

--- src/pages/entry/ScoopOption.jsx
+++ src/pages/entry/ScoopOption.jsx
@@ -3,11 +3,11 @@ import Col from "react-bootstrap/Col";
 export default function ScoopOption({ name, imagePath }) {
     return (
         <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
-            <image
+            <img
                 src={`http://localhost:3030/${imagePath}`}
                 alt={`${name} scoop`}
-                style={{ width: '75%' }}
-            ></image>
+                style={{ width: "75%" }}
+            ></img>
         </Col>
     );
 }
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -6,7 +6,7 @@ test("displays image for each scoop from the server", async () => {
 
     // find images
     const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
-    expect(scoopImages).toHaveLength(2); // because we have 2 in our handlers array
+    expect(scoopImages).toHaveLength(2); // because we have 2 in our handlers array 
 
     // confirm alt text of images
     const altText = scoopImages.map((element) => element.alt);
